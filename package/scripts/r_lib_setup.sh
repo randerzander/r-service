615d45a51489ed7d40ae346a50a7f3f98fa5775d
Randy Gelhausen <rgelhau@gmail.com>
2015-04-12 00:12:55 -0400

    Fixed reference to hadoop packages prior to dep installs

--- package/scripts/r_lib_setup.sh
+++ package/scripts/r_lib_setup.sh
@@ -7,10 +7,6 @@ R_LIBS_DIR=$3
 export HADOOP_CMD=/usr/bin/hadoop
 export HADOOP_STREAMING=/usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar
 
-R CMD INSTALL $FILES_DIR/rmr2_3.3.0.tar
-R CMD INSTALL $FILES_DIR/rhdfs_1.0.8.tar.gz
-R CMD INSTALL $FILES_DIR/plyrmr_0.5.0.tar.gz
-
 for filename in $FILES_DIR/libs/*.tgz
 do
   tar -xzvf $filename -C $R_LIBS_DIR
